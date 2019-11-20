af281f62a4f801b2fe6609fa10d4b811a098996e
darylnak <darylnak@no-reply.github.com>
2019-11-19 17:25:13 -0800

    add database capability and added floor based description to Add POI

--- app/src/main/java/devdaryl/com/mapwithlogin/MapActivity.java
+++ app/src/main/java/devdaryl/com/mapwithlogin/MapActivity.java
@@ -2,15 +2,25 @@ package devdaryl.com.mapwithlogin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
@@ -25,6 +35,7 @@ public class MapActivity extends AppCompatActivity implements NavigationView.OnN
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
     private DrawerLayout getmDrawerLayout;
+    FirebaseFirestore mFirestore;
 
 
     @Override
@@ -34,6 +45,7 @@ public class MapActivity extends AppCompatActivity implements NavigationView.OnN
 
         mNavigationView = (NavigationView) findViewById(R.id.nav_view);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mFirestore = FirebaseFirestore.getInstance();
 
         if (mNavigationView != null) {
             mNavigationView.setNavigationItemSelectedListener(this);
@@ -46,6 +58,68 @@ public class MapActivity extends AppCompatActivity implements NavigationView.OnN
 //                openLoginActivity();
 //            }
 //        });
+    }
+
+    // Usage: pulls a list of the document id's under that key_word
+    private void locationKeyWord(final String key_word) {
+        mFirestore.collection("key_words").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                boolean bool = false;
+                if(task.isSuccessful()) {
+                    List<DocumentSnapshot> list = task.getResult().getDocuments();
+                    for(DocumentSnapshot document: list){
+                        if(document.get(key_word) != null) {
+                            List<String> kw_list = (List<String>) document.get(key_word);
+                            printKeyWordLocations(kw_list);
+                            Toast.makeText(MapActivity.this, "KeyWord list retrieved from Firestore", Toast.LENGTH_LONG).show();
+                            bool = true;
+                            break;
+                        }
+                    }
+                    if(!bool){
+                        Toast.makeText(MapActivity.this, "Not in Firestore", Toast.LENGTH_LONG).show();
+
+                    }
+                    //DocumentSnapshot snapshot = task.getResult();
+                    //String song = snapshot.getString("")
+                }
+                else {
+                    Log.d("Firebase Error", "Error: " + task.getException().getMessage());
+
+                }
+            }
+        });
+    }
+    /*
+     * Usage: List contains strings, which are the document id's for locations in the "location" collection on Firestore
+     */
+    private void printKeyWordLocations(List<String> list) {
+        for(String document_id: list) {
+            mFirestore.collection("locations").document(document_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        //Map<String, Object> document = task.getResult().getData();
+                        if(document.exists()){
+                            Map<String, Object> doc = task.getResult().getData();
+                            Toast.makeText(MapActivity.this, "Item in key_word is: " + doc.get("name"), Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Log.d( "No such document", "Error");
+                        }
+                        Toast.makeText(MapActivity.this, "KeyWord list retrieved from Firestore", Toast.LENGTH_LONG).show();
+
+                        //DocumentSnapshot snapshot = task.getResult();
+                        //String song = snapshot.getString("")
+                    } else {
+                        Log.d("Firebase Error", "Error: " + task.getException().getMessage());
+
+                    }
+                }
+            });
+        }
     }
 
     public void openLoginActivity() {
